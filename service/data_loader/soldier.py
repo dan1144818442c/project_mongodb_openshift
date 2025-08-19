380b42a952bc824c24f5fc40045ee188e38a1074
d.c! <daniel.c99999@gmail.com>
2025-08-19 17:18:27 +0300

    add class soldier

---
+++ service/data_loader/soldier.py
@@ -0,0 +1,7 @@
+class soldier:
+    def __init__(self , id , first_name ,last_name , rank , phone_number):
+        self.id  = id
+        self.first_name = first_name
+        self.last_name = last_name
+        self.rank = rank
+        self.phone_number = phone_number
